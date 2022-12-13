f29d777e9aef77b716308db00f3e729906a762aa
Julie Iben Kristensen <iben.kristensen98@hotmail.com>
2022-12-13 21:37:30 +0100

    done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
